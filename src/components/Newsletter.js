e0091d259867213502c7abd425ad63b97b5aed00
vipin kumar <vipinkumar@Parminders-Mac-mini.local>
2018-09-11 13:27:43 +0530

    test seo tag

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import theme from '../utils/theme';
 import NewsletterForm from './NewsletterForm';
 
 const Container = styled.section`
