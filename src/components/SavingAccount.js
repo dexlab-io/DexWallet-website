6f4cb280805840fc288d7bd9a0a6c946bd205a7b
alexintosh <alessio.d@gmail.com>
2019-05-08 17:44:17 +0200

    We can't english, part2

--- src/components/SavingAccount.js
+++ src/components/SavingAccount.js
@@ -5,7 +5,7 @@ const SavingAccount = () => {
   return (
     <PictureLeft
       title="As easy as a saving account, with up to 3x more gains"
-      subtitle="On DexWallet, Liquidity supply interest rates are usually from 2 to 5 times higher than what offered by your bank"
+      subtitle="On DexWallet, Liquidity supply interest rates are usually from 2 to 5 times higher than what's offered by your bank"
       url="/img/savingAccount.jpg"
     />
   );
