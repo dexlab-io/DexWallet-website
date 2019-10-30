6bf5fa5177f97d22eab844d0b28dcf2d351da7e7
Nicola Di Marco <dimarco.nicola@gmail.com>
2019-10-30 13:29:23 +0100

    Moonpay CTA referral

--- src/components/MoonPay.js
+++ src/components/MoonPay.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import PictureRight from './PictureRight';
+import MoonplayLayout from './MoonplayLayout';
 
 const MoonPay = () => {
   return (
-    <PictureRight
+    <MoonplayLayout
       title="Buy Crypto seamlessly using your Credit Card"
       subtitle="Integrated feature allows users to buy Ether with Credit Card through MoonPay to make the journey simpler, faster and safer for everyone."
       url="/img/MoonPay.jpg"
