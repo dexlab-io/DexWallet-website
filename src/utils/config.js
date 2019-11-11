595b3d6be58baa57f681269e143a2324f8deb87c
Nicola Di Marco <dimarco.nicola@gmail.com>
2019-11-11 15:20:15 -0500

    image preview for socials

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,14 @@
 module.exports = {
   DEBUG: process.env.NODE_ENV === 'development',
 
-  siteName: 'DexWallet - The only Wallet you need by Dexlab',
-  author: 'Parminder Klair',
-  description: 'The Ethereum Wallet you need by Dexlab',
+  siteName: 'The mobile wallet for decentralized finance',
+  author: 'Dexwallet.io',
+  description:
+    'Borrowing, Lending, Exchange and native in-wallet Staking for everyone. Start building your passive income strategy now',
   siteUrl: 'https://www.dexwallet.io/',
   image: 'https://www.dexwallet.io/images/preview.png',
 
-  twitter: '@dexlab_io',
+  twitter: '@Dexwallet',
   fbUserId: '@NAME-HERE',
   fbAppID: '@NAME-HERE',
   type: 'website',
