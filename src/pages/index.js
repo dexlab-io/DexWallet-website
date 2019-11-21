7832628880eecf511b73330fe156d556960f90c2
Nicola Di Marco <dimarco.nicola@gmail.com>
2019-11-21 10:18:59 -0500

    Spread out the testimonials/social proof in the page. #69 (#72)

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ import Hero from '../components/Hero';
 import BigX from '../components/BigX';
 import BigPhone from '../components/BigPhone';
 import Whitelabel from '../components/Whitelabel';
-import Quotes from '../components/Quotes';
 import Staking from '../components/Staking';
 import Integrations from '../components/Integrations';
 import SavingAccount from '../components/SavingAccount';
@@ -17,6 +16,7 @@ import WlFeatures from '../components/WlFeatures';
 import MediumPosts from '../components/MediumPosts';
 import NewsletterForm from '../components/NewsletterForm';
 import MoonPay from '../components/MoonPay';
+import TeamMember from '../components/TeamMember';
 
 export default class IndexPage extends React.Component {
   render() {
@@ -33,12 +33,43 @@ export default class IndexPage extends React.Component {
         <Hero />
         <BigPhone />
         <Integrations />
+        <TeamMember
+          name="Michael Dunworth - CEO Wyre"
+          description={
+            "DexWallet gets it. The product is beautiful and isn't just a wallet. It's a lens into your financial machine that's working for you. Returns, loans, exchanging, if it's not the UI for the next 100M, then whatever will be is going to be damn close."
+          }
+          url="/img/michael.jpg"
+          alt="Michael Dunworth"
+        />
         <Staking />
+        <TeamMember
+          name="Tom Bean - CEO BZX"
+          description="The DexWallet app provides a slick interface and intuitive gateway to decentralized finance. The team behind it is talented and knows what they are doing. I look forward to partnering with them while we work together to build out the future of finance, and unleash the power of the blockchain."
+          url="/img/tom.jpg"
+          alt="Tom Bean"
+        />
         <SavingAccount />
+        <TeamMember
+          name="Kain Warwick - Founder Synthetix"
+          description="A mobile native integration of defi protocols is critical to increasing accessibility of DeFi."
+          url="/img/kain.jpg"
+          alt="Kain Warwick"
+        />
         <MoonPay />
+        <TeamMember
+          name="Patricio Worthalter - CEO Milliwatt"
+          description="I truly believe that wallets are a major component in the road to adoption and mainstream use of cryptocurrencies and Dexwallet has positioned as an agile user-focused product."
+          url="/img/patricio.jpg"
+          alt="Patricio Worthalter"
+        />
         <NativedApps />
         <Payments />
-        <Quotes />
+        <TeamMember
+          name="Gabriedle Rigo - CEO RigoBlock"
+          description="DexWallet is a gateway to the decentralized world, reducing friction for the user, a much needed app for crypto adoption."
+          url="/img/rigo.jpg"
+          alt="Gabriedle Rigo"
+        />
         <Whitelabel />
         <WlFeatures />
         <MediumPosts />
