3ddcb214dd55cfe6164401bc79190828d0036904
Nicola Di Marco <dimarco.nicola@gmail.com>
2019-12-03 13:49:38 -0500

    Replacing team video (#75)
    * adding youtube video
    
    * replacing video and hiding title on mobile

--- src/components/TeamVideo.js
+++ src/components/TeamVideo.js
@@ -5,15 +5,15 @@ const TeamVideo = props => {
     <section className="sectionComponentLeft">
       <span className="sectionPicture">
         <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube-nocookie.com/embed/RHkcMzRaw5U"
+          width="100%"
+          height="300"
+          src="https://www.youtube.com/embed/Qk-SgH0Bvhg"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowfullscreen="true"
         />
       </span>
-      <span className="columnText">
+      <span className="columnText noshow">
         <span className="leftContentText">
           <h1 className="sectionTitle">Meet the team</h1>
           <h2 className="sectionSub">
